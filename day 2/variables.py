56a330f7cee80552ba54248573c420b7393a3692
Hajjaju <hauwa.zyibrahim@gmail.com>
2023-11-30 18:36:42 +0000

    i added day 2

---
+++ day 2/variables.py
@@ -0,0 +1,56 @@
+# Day 2: 30 Days of python programming
+# Exercise: Level 1
+first_name = 'Hauwau'
+last_name = 'Zakariyau'
+full_name = 'Hauwau', 'Zakariyau'
+country = 'Nigeria'
+city = 'Bauchi'
+age = 26
+year = 2023
+is_married = True
+is_true = 'Yes'
+is_light_on = 'No'
+personal_info = {'name':'Hauwau', 'country':'Nigeria', 'city':'Bauchi'}
+print('first name:', first_name)
+print('last name:', last_name)
+print('full name:', full_name)
+print('country:', country)
+print('city:', city)
+print('age:', age)
+print('year:', year)
+print('is married::', is_married)
+print('is true:', is_true)
+print('is light on:', is_light_on)
+print('personal info:', personal_info)
+
+# Exercises: Level 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(personal_info))
+print('first name lenght:', len(first_name))
+print(len('first_name') < len('last_name'))
+num_one = 5
+num_two = 4
+num_one + num_two
+total = 9
+num_one - num_two
+diff = 1 
+num_two * num_one
+product = 20
+num_one / num_two
+division = 1.25
+num_two // num_one
+remainder = 1
+num_one ^ num_two
+area_of_a_circle = 2827.8
+circum_of_circle = 188.52
+
+
