7403db90d3130a29b4efecac45f02823f50da2c7
Hajjaju <143223216+Hajjaju@users.noreply.github.com>
2023-12-06 12:59:35 +0000

    Add files via upload

---
+++ Day1_Helloworldd.py
@@ -0,0 +1,29 @@
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+# checking data types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4))
+print(type( {'Asabena', 'Python', 'Finland'}))
+print(type('Hauwau'))
+print(type('Zakariyau'))
+print(type('Nigeria'))
+
+
+# Different data types
+# Number
+# integer (-5, -2, 0, 2, 5,)
+# float (-1.5, 0.0, 1.5, 3.5)
+# complex (2+4x, 3+3y, 2+y)
+# String 'Data science' "I am enjoying python"
+# Booleans true or false
+# List ['rice' 'beans' 'maize']
+# Tuple ('january', 'february', 'march')
+# Set{8, 4, 9, 5}
+# Dictionary {'school_name':'ArewaDS'}
