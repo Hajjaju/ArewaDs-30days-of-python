188ad52144b472609c7ed10c662517e4e6d37929
Hajjaju <143223216+Hajjaju@users.noreply.github.com>
2023-12-06 12:54:21 +0000

    Add files via upload

---
+++ Day4_Strings.py
@@ -0,0 +1,73 @@
+# Day 4: 30 days of python
+# Exercises - Day 3
+duration = 'Thirty'
+time = 'days'
+con = 'of'
+programme = 'python'
+space = ' '
+course = duration + space + time + space + con + space + programme
+print(course)
+code = 'coding'
+to = 'for'
+people = 'all'
+sentence = code + space + to + space + people
+print(sentence)
+company = "Coding for All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+first_word = company[0:6]
+print(first_word)
+print(company.find('coding'))
+print(company.replace('coding', 'Python'))
+challenge = 'Python for Everyone'
+print(challenge.replace('everyone', 'all'))
+print(company.split(' '))
+media = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon,"
+print(media.split(', '))
+first_later = company[0]
+print(first_later)
+last_index = len(company) - 1
+index_ten = company[10]
+print(last_index)
+print(index_ten)
+cfa=company [0:8:12]
+print(cfa)
+print(company.startswith('coding'))
+print(company.endswith('coding'))
+institute = 'Coding for all People'
+print(institute.rfind('l'))
+new_sentence = 'You cannot end a sentence with because because because is a conjuction'
+sub_string = 'because'
+print(new_sentence.index(sub_string))
+print(new_sentence.rindex(sub_string))
+python_libraries = ['Django', 'Flask', 'Bottle', 'Pyramid', 'Falcon']
+libraries = '# '.join(python_libraries)
+print(libraries)
+print('I am enjoying this challenge. \nI just wonder what is next.')
+print('Name\tAge\tCountry\tCity')
+print('Asabeneh\t250\tFinland\tHelsinki')
+a = 8
+b = 6
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b))
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a** b))
+radius = 10
+pi = 3.14
+area = pi * radius ** 2
+formated_string = 'the area of a circle with a radius {} is {:.2f}.'.format(radius, area)
+print(formated_string)
+
+
+
+
+
+
