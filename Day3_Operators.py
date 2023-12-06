1bfa5665aa14ba5d70de292a799c8b1974875316
Hajjaju <143223216+Hajjaju@users.noreply.github.com>
2023-12-06 12:52:20 +0000

    Add files via upload

---
+++ Day3_Operators.py
@@ -0,0 +1,40 @@
+# Day 3:30 days of python
+# Exercise - Day 3
+Age = 26
+Height = 53.5
+# for area of a triangle
+base = 20
+height = 10
+area_of_triangle = 0.5*base*height
+print('Area of triangle:', area_of_triangle)
+# for perimeter of triangle
+a = 5
+b = 4 
+c = 3
+perimeter_of_triangle = a + b + c 
+print('Perimeter of triangle:',perimeter_of_triangle)
+# for a rectangle
+lenght = 10
+width = 7
+area_of_rectangle = lenght * width
+perimeter_of_rectangle = 2*(lenght*width)
+print('Area of rectangle:', area_of_rectangle)
+print('Perimeter of rectangle:', perimeter_of_rectangle)
+# for a circle
+radius = 10
+pi = 3.14
+Area_of_circle = pi*radius*radius
+Circumference_of_circle = 2*pi*radius
+print('Area of circle:', Area_of_circle)
+print('circumference of circle:', Circumference_of_circle)
+print('y=2x-2')
+print(len('python')==len('dragon'))
+print('dragon and python:', 'on')
+
+
+
+
+
+
+
+
